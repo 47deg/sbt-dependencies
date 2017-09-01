eb0dcd907eed0cb07273e39ccedf0cd2722f5e78
Suhas Gaddam <suhas.g.2011@gmail.com>
2017-09-01 14:48:06 -0700

    Port plugin to sbt {1.0.x, 0.13.16} [wip]

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,5 +1,5 @@
 import com.typesafe.sbt.site.SitePlugin.autoImport._
-import microsites.MicrositeKeys._
+// import microsites.MicrositeKeys._
 import sbt.Keys._
 import sbt._
 import sbtorgpolicies.OrgPoliciesPlugin
@@ -7,7 +7,7 @@ import sbtorgpolicies.OrgPoliciesPlugin.autoImport._
 import sbtorgpolicies.model._
 import sbtorgpolicies.runnable.syntax._
 import sbtorgpolicies.templates.badges._
-import tut.Plugin.tut
+// import tut.Plugin.tut
 
 object ProjectPlugin extends AutoPlugin {
 
@@ -21,38 +21,39 @@ object ProjectPlugin extends AutoPlugin {
       sbtPlugin := true,
       libraryDependencies ++= Seq(
         %%("github4s"),
-        "com.47deg" %% "org-policies-core" % "0.4.17"),
-      addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.0")
+        "com.47deg" %% "org-policies-core" % "0.6.0-SNAPSHOT"),
+      addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.1")
     )
 
-    lazy val micrositeSettings = Seq(
-      micrositeName := "sbt-dependencies",
-      micrositeDescription := "An SBT plugin that allows to you to keep your project dependencies up-to-date",
-      micrositeBaseUrl := "sbt-dependencies",
-      micrositeDocumentationUrl := "/sbt-dependencies/docs/",
-      micrositeGithubOwner := "47deg",
-      micrositeGithubRepo := "sbt-dependencies",
-      micrositeHighlightTheme := "darcula",
-      micrositePalette := Map(
-        "brand-primary"   -> "#005b96",
-        "brand-secondary" -> "#03396c",
-        "brand-tertiary"  -> "#011f4b",
-        "gray-dark"       -> "#48474C",
-        "gray"            -> "#8D8C92",
-        "gray-light"      -> "#E3E2E3",
-        "gray-lighter"    -> "#F4F3F9",
-        "white-color"     -> "#FFFFFF"
-      ),
-      includeFilter in makeSite := "*.html" | "*.css" | "*.png" | "*.jpg" | "*.gif" | "*.js" | "*.swf" | "*.md"
-    )
+    // lazy val micrositeSettings = Seq(
+    //   micrositeName := "sbt-dependencies",
+    //   micrositeDescription := "An SBT plugin that allows to you to keep your project dependencies up-to-date",
+    //   micrositeBaseUrl := "sbt-dependencies",
+    //   micrositeDocumentationUrl := "/sbt-dependencies/docs/",
+    //   micrositeGithubOwner := "47deg",
+    //   micrositeGithubRepo := "sbt-dependencies",
+    //   micrositeHighlightTheme := "darcula",
+    //   micrositePalette := Map(
+    //     "brand-primary"   -> "#005b96",
+    //     "brand-secondary" -> "#03396c",
+    //     "brand-tertiary"  -> "#011f4b",
+    //     "gray-dark"       -> "#48474C",
+    //     "gray"            -> "#8D8C92",
+    //     "gray-light"      -> "#E3E2E3",
+    //     "gray-lighter"    -> "#F4F3F9",
+    //     "white-color"     -> "#FFFFFF"
+    //   ),
+    //   includeFilter in makeSite := "*.html" | "*.css" | "*.png" | "*.jpg" | "*.gif" | "*.js" | "*.swf" | "*.md"
+    // )
 
   }
 
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
       name := "sbt-dependencies",
-      scalaVersion := "2.10.6",
-      crossScalaVersions := Seq("2.10.6"),
+      scalaVersion := scalac.`2.12`,
+      crossScalaVersions := Seq(scalac.`2.12`),
+      crossSbtVersions := Seq(sbtV.`0.13`, sbtV.`1.0`),
       scalaOrganization := "org.scala-lang",
       startYear := Option(2017),
       orgGithubTokenSetting := "GITHUB_TOKEN",
@@ -60,7 +61,7 @@ object ProjectPlugin extends AutoPlugin {
       orgScriptTaskListSetting := List(
         orgValidateFiles.asRunnableItem,
         (compile in Compile).asRunnableItem,
-        (tut in ProjectRef(file("."), "docs")).asRunnableItem
+        // (tut in ProjectRef(file("."), "docs")).asRunnableItem
       ),
       orgBadgeListSetting := List(
         TravisBadge.apply(_),
